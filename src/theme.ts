78db4a8b3f94ed9ad6ea0643f932c134d7594202
Hunter Carlis <huntercarlis@yahoo.com>
2023-11-08 15:38:42 -0800

    customize theme for darker greys

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,22 @@ const config: ThemeConfig = {
     initialColorMode: 'dark'
 };
 
-const theme = extendTheme ({ config});
+const theme = extendTheme ({ 
+    config,
+    colors: {
+    gray: {
+        50: '#f9f9f8',
+        100: '#ededef',
+        200: '#d3d3d2',
+        300: '#b3b3b4',
+        400: '#a0a0a1',
+        500: '#898988',
+        600: '#6c6c5c',
+        700: '#202021',
+        800: '#121211',
+        900: '#111'
+    }
+    }
+});
 
 export default theme;
